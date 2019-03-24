11944077f1b8044f6b7198e55df21c6e86d6bf24
matteo <matteo.barbera97@gmail.com>
2019-03-24 17:24:54 +0100

    added functions that outputs a new drone heading added function that checks whether edge of CyberZoo has been reached

--- obs_black_filt.py
+++ obs_black_filt.py
@@ -1,4 +1,5 @@
-import os
+from os import listdir
+import math as m
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,9 +48,9 @@ def find_safe_vertical(sum_arr, line_skip=1):
     Finds longest horizontal sequence of 0s in 2D array and returns the index
     of the middle of said sequence
 
-    :param sum_arr: 2D array containing zeros
+    :param sum_arr: 2D array representing subsampled filtered image
     :param line_skip: tunes how many lines the func will search
-    :return: row, col index of middle of longest sequence
+    :return: row, col index of middle of longest sequence of 0s
     """
     y_tot, _ = sum_arr.shape
     seq_len = 0
@@ -85,7 +86,52 @@ def find_safe_vertical(sum_arr, line_skip=1):
     return seq_line, (seq_start + seq_end) // 2
 
 
-for image in os.listdir('.//pics'):
+def new_heading(arr_shape, vertical, fov_hor=120):
+    """
+    Translates pixel distance from center of image into an angle (heading)
+
+    :param arr_shape: shape of sum_arr
+    :param vertical: column of safe pixel
+    :param fov_hor: horizontal FoV of camera
+    :return: new heading
+    """
+    px_away_from_center = abs(vertical - arr_shape[1]/2)
+    angle_from_center = 0.5*fov_hor * (px_away_from_center / (arr_shape[1]/2))
+    return angle_from_center  # currently in degrees
+
+
+def edge_reached_check(sum_arr, safe_point, h, max_sum=72, fov_ver=60, d_margin=1.5):
+    """
+    Checks if drone is too close to the edge of the CyberZoo
+
+    :param sum_arr: 2D array representing subsampled filtered image
+    :param safe_point: (row, col) of safe point (from find_safe_vertical)
+    :param h: current height of drone
+    :param max_sum: maximum value each element of sum_arr can have
+    :param fov_ver: vertical FoV of camera
+    :param d_margin: distance from edge in meters added as margin
+    :return: boolean
+    """
+    arr_shape = sum_arr.shape
+    d_img_bot = h * m.tan(m.radians(90 - 0.5*fov_ver))
+    theta = m.atan2(h, d_img_bot + d_margin)
+    theta_px = 0.5*arr_shape[0] * (theta / 0.5*fov_ver)
+
+    white_idx = 0
+    for idx in range(safe_point[0], arr_shape[0]):
+        if sum_arr[idx, safe_point[1]] == max_sum:
+            white_idx = idx
+            break
+    # What happens if no white patch is found?
+
+    edge_reached = False
+    if white_idx > theta_px:
+        edge_reached = True
+
+    return edge_reached
+
+
+for image in listdir('.//pics'):
     img = cv2.imread('.//pics//' + image)
     img = np.rot90(img)
 
@@ -93,6 +139,7 @@ for image in os.listdir('.//pics'):
     white_sum = find_white(filt_img, grid_size=(30, 54))
 
     row, col = find_safe_vertical(white_sum)
+    print(new_heading(white_sum.shape, col))  # degrees
 
     cv2.imshow('image', img)
     cv2.imshow('filt_img', filt_img)
@@ -102,6 +149,7 @@ for image in os.listdir('.//pics'):
     ax = fig.add_subplot(1, 1, 1)
     plt.imshow(white_sum, cmap='gnuplot2')
     ax.add_patch(plt.Circle((col, row), 0.5, color='r', fill=False))
+
     plt.draw()
     plt.waitforbuttonpress(0)
     plt.close()
